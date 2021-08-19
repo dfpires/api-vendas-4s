be6fbca22559fe8632fc99cf14587b135db702a0
Daniel Pires <dfpires@gmail.com>
2021-08-18 22:03:00 -0300

    18-08

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository } from "typeorm";
+import Product from "../typeorm/entities/Product";
+import ProductRepository from "../typeorm/repositories/ProductRepository";
+
+
+// criar uma inteface -> tipo de dado
+interface IRequest {
+    name: string,
+    price: number,
+    quantity: number
+}
+
+// criar uma classe 
+class CreateProductService {
+
+    // criar um método de execução da criação do Produto
+    public async execute({name, price, quantity} : IRequest): Promise<Product>{
+        // recupera um repositório do produto
+       let productRepository = getCustomRepository(ProductRepository);
+        // verifica se o produto existe
+       let productExist = await productRepository.findByName(name);
+       if (productExist){
+           // não podemos cadastrar
+           console.log(`Produto já existe no banco de dados`)
+            // lançar uma exceção
+       }
+       // produto não existe
+       // vamos criar
+       let newProduct = productRepository.create({
+           name, 
+           price, 
+           quantity
+       })
+       // vamos salvar no banco
+       await productRepository.save(newProduct)
+       return newProduct;
+    }
+}
+
+export default CreateProductService
