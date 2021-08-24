eb0d7f61cdf154fe8a9f7ce3a91876e89a4c59e7
Daniel Pires <dfpires@gmail.com>
2021-08-23 21:38:38 -0300

    23-08

---
+++ src/shared/errors/AppErrors.ts
@@ -0,0 +1,13 @@
+
+
+class AppError {
+    public message: string;
+    public statusCode: number;
+
+    constructor(message: string, statusCode: number){
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+export default AppError;
