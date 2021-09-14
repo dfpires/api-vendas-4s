6c564d9ece06fece905feeb86be0e54eb23880b2
Daniel Pires <dfpires@gmail.com>
2021-09-13 21:46:18 -0300

    13-09

--- src/modules/users/services/SessionUserService.ts
+++ src/modules/users/services/SessionUserService.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import AppError from "../../../shared/errors/AppErrors";
 import User from "../typeorm/entities/User";
 import UserRepository from "../typeorm/repositories/UserRepository";
+import {sign} from 'jsonwebtoken'
 
 // interface de requisição de dados
 interface IRequest {
@@ -26,6 +27,20 @@ class SessionUserService {
 
         // compara a senha informada com a senha do banco de dados (está em md5)
         let passwordConfirmed = await compare(password, user.password)
+
+        if (!passwordConfirmed){ // senha não confere
+            throw new AppError(`Incorrect email / password combination`, 401)
+        }
+
+        let token = sign({}, '574911d7c7b787c217319ea068a230f8', {
+            subject: user.id,
+            expiresIn: '1d'
+        })
+
+        return {
+            user, 
+            token
+        }
     }
 }
 
