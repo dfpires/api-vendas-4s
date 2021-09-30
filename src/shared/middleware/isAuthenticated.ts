e3b7c1f0b6fcd3a48846e3d5026b0ed3416faeae
Daniel Pires <dfpires@gmail.com>
2021-09-29 22:12:56 -0300

    29-09

--- src/shared/middleware/isAuthenticated.ts
+++ src/shared/middleware/isAuthenticated.ts
@@ -3,6 +3,13 @@ import { verify } from "jsonwebtoken";
 import AppError from "../errors/AppErrors";
 import authConfig from '../../config/auth'
 
+// vamos criar uma interface, que é um tipo de dados
+interface ITokenPayload {
+    iat: number,
+    exp: number,
+    sub: String
+}
+
 export default function isAuthenticated(request: Request, response: Response, next: NextFunction): void {
     
     // obter a autorização que está dentro do cabeçalho da requisição
@@ -22,6 +29,21 @@ export default function isAuthenticated(request: Request, response: Response, ne
     // tratar erro
     try { // tentar verificar se o token é valido
         let decodedToken = verify(token, authConfig.jwt.secret)
+        // estamos mostrando o token do usuário decodificado
+        console.log(decodedToken)
+        /* 
+        {
+            iat: 1632962410,
+            exp: 1633048810,
+            sub: '5aafd764-aacd-4454-b315-92ceca43af2f'
+        }
+        */
+        // no campo sub, temos o id do usuário, que requemos passar na requisição
+        const {sub} = decodedToken as ITokenPayload // agora sim conseguimos pegar apenas o campo sub
+
+        // vamos alterar as informações da requisição - a requisição precisa conter o id do usuário
+
+
         return next() // deixa a API ser utilizada -> será consumida
     }
     catch { // se o token não for válido
